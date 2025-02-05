e0e17319c3e89db2c88d4fcbb2592a3b16c275a6
Rajnandini-Poddar <nandinipoddar95@gmail.com>
2025-02-06 01:40:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const music_list = [
         img : 'images/stay.png',
         name : 'Stay',
         artist : 'The Kid LAROI, Justin Bieber',
-        music : 'music/stay.mp3'
+        music : 'music/Stay.mp3'
     },
     {
         img : 'images/fallingdown.jpg',
